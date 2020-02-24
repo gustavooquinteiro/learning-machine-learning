b575623769dcfb5645299c9e74f537bb2e6defc8
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2020-02-24 17:21:37 -0300

    List data from unique target groups

--- text_analysis/20newsgroups.py
+++ text_analysis/20newsgroups.py
@@ -1,8 +1,13 @@
 from sklearn.datasets import fetch_20newsgroups
+import numpy as np
 
 def main():
-    groups=fetch_20newsgroups()
-    print(groups)
+    groups = fetch_20newsgroups()
+    np.unique(groups.target)
+    for i in range(len(groups.target)):
+        print("Target {} is {}" .format(groups.target[i], groups.target_names[groups.target[i]]))
+        print("Data: ")
+        print(groups.data[i])
     
 if __name__=='__main__':
     main()
